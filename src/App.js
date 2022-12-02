b68002996464bc164a97714af3077d680e736ea9
Tony Creavin <tonycreavin@gmail.com>
2022-12-02 13:27:51 +0100

    Initial commit, login page with token and session storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Preferences from './components/Preferences';
+import Login from './components/Login/Login';
+
+function setTokenToSessionStorage(userToken) {
+  sessionStorage.setItem('token', userToken.token);
+}
 
 function App() {
+  const [token, setToken] = useState(() => sessionStorage.getItem('token'));
+
+  useEffect(() => {
+    if (token) {
+      setTokenToSessionStorage(token);
+    }
+  }, [token]);
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <button
+        onClick={() => {
+          setToken('');
+          setTokenToSessionStorage('');
+        }}
+      >
+        logout
+      </button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/preferences" element={<Preferences />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
